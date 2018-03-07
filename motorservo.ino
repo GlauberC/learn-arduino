2871f2bff497c915d7202606121e075ea89d0f0e
Glauber Carvalho <32900021+GlauberC@users.noreply.github.com>
2018-03-07 15:18:49 -0300

    MotorServo
    Código para executar um motor servo utilizando arduino

---
+++ motorservo.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>//biblioteca para o servo motor
+Servo myservo; //objetos da classe servo
+
+void setup() {
+  // put your setup code here, to run once:
+  myservo.attach(10); //indica que myservo será na porta 10
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  myservo.write(0);
+  delay(1000);
+  myservo.write(90);
+  delay(1000);
+  myservo.write(180);
+  delay(1000);
+
+}
