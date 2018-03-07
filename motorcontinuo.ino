b186e872c7b3f730165af7e4ed003a6bbc3e6bf8
Glauber Carvalho <32900021+GlauberC@users.noreply.github.com>
2018-03-07 15:31:19 -0300

    motorContinuo
    Exemplo do uso de motor contínuo

---
+++ motorcontinuo.ino
@@ -0,0 +1,15 @@
+  #include <Servo.h>//biblioteca para o servo motor
+  Servo myservo, mys; //objetos da classe servo
+  void setup()
+  {
+      myservo.attach(10); //indica que myservo será na porta 10
+  }
+  void loop()
+  {
+     myservo.writeMicroseconds(2000); //horário
+     delay(1000);
+     myservo.writeMicroseconds(1000); //anti horário
+     delay(1000);
+     myservo.writeMicroseconds(1500); //parado
+     delay(2000);
+  }
